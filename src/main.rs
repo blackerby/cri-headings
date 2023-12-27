23f07b8a4d304f4defb0cef595cfc66c59c94e83
blackerby <wmblackerby@gmail.com>
2023-12-27 15:21:15 -0600

    complete first draft

--- src/main.rs
+++ src/main.rs
@@ -14,21 +14,29 @@ fn main() -> Result<()> {
         _ => {}
     }
 
+    // read input
     let file_name = &args[1];
     let file = File::open(file_name)?;
     let mut buf_reader = BufReader::new(file);
     let mut html = String::new();
     buf_reader.read_to_string(&mut html)?;
-    let _document = Html::parse_document(&html);
-    let _selector = Selector::parse("p.heading").unwrap();
+    let document = Html::parse_document(&html);
+    let selector = Selector::parse("p.heading").unwrap();
 
-    let file_stem = Path::new(file_name).file_name().unwrap();
+    // setup output file
+    let file_stem = Path::new(file_name).file_stem().unwrap();
     let output_filename = format!("{}_headings.txt", file_stem.to_str().unwrap());
     let output_file = File::create(output_filename)?;
     let mut buf_writer = BufWriter::new(output_file);
 
-    for heading in _document.select(&_selector) {
-        writeln!(&mut buf_writer, "{}", heading.inner_html())?;
+    // write output
+    for p_tag in document.select(&selector) {
+        let raw_heading = p_tag.inner_html();
+        let heading = match raw_heading.find('<') {
+            None => raw_heading,
+            Some(i) => raw_heading[..i].to_owned(),
+        };
+        writeln!(&mut buf_writer, "{}", heading)?;
     }
     Ok(())
 }
